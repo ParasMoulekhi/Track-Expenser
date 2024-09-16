014d9b15e25c9b8b3fedfb95e211994e83933fea
Paras <parasmoulekhi11@gmail.com>
2024-09-16 10:20:50 +0530

    change background color

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -20,6 +20,7 @@ export default function ExpenseTable({
   const total = filterExpenses.reduce((acc, el) => {
     return acc + parseInt(el.amount);
   }, 0);
+
   return (
     <>
       <ContextMenu
